bb1779d2458dd94956647af8d1dfaed97aff386b
Alexander Grace <alexander.grace@chasacademy.se>
2018-02-01 16:02:00 +0100

    add search and render images from flickr api functionality

--- src/js/app.js
+++ src/js/app.js
@@ -3,84 +3,75 @@ import 'popper.js';
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
 /*the axel function which turns jsobject into a long url string.
-  i want to learn this to i should brush up on js array manipulation and objekt.key function */ 
+  i want to learn this to i should brush up on js array manipulation and objekt.key function */
+
 import { axelVisar } from './axelmachine.js';
-
-
-
 
 //doesnt work as class property so i have the searchinput globally(does this depend on webpack?)
 
-
 class MashedApi {
 	constructor(element) {
-        let _this = this;
-        this.root = element;
-        this.addEventListeners();
-    }
+		let _this = this;
+		this.root = element;
+		this.addEventListeners();
+	}
 
-    
-    
-
-    addEventListeners() {
-        const search = document.querySelector('#search-query');
+	addEventListeners() {
+		const search = document.querySelector('#search-query');
 		const searchButton = document.querySelector('#search-button');
-        searchButton.addEventListener('click', () => {
-                this.fetchFlickrRequest(search.value);
-                //this.fetchSynonymsRequest(search.value);
-        });
-        
-    }
+		searchButton.addEventListener('click', () => {
+			this.fetchFlickrRequest(search.value);
+			//this.fetchSynonymsRequest(search.value);
+		});
+	}
 
-      getSearchQuery() {
+	getSearchQuery() {
 		let query = search.value;
 		if (!query.length) {
 			return;
 		}
-    } 
+	}
 
-    renderFlickrImages(images) {
-        images.map(image = () => {
+	renderFlickrImages(images) {
+		const imageContainer = document.querySelector('.container');
+		imageContainer.innerHTML = '';
+		images.map(function(pic) {
+			let image = new Image(200, 200);
+			let link = document.createElement('a');
+			link.setAttribute('href', `${pic.url_o}`);
+			image.src = `${pic.url_o}`;
+			link.append(image);
+			return imageContainer.appendChild(link);
+		});
+	}
 
-        });
+	fetchFlickrRequest(input) {
+		let flickrAPIkey = '3217401972bbfbe982ebee49a2300045';
+		let resourceUrl = `https://api.flickr.com/services/rest/?`;
 
-    }
+		let params = {
+			method: 'flickr.photos.search',
+			api_key: flickrAPIkey,
+			text: input,
+			sort: 'relevance',
+			extras: 'original_format, url_o, url_q',
+			license: '2,4,5,6,7',
+			per_page: 12,
+			format: 'json',
+			nojsoncallback: 1,
+			dimension_search_mode: 'max'
+		};
 
-    fetchFlickrRequest(input){
-    let flickrAPIkey = process.env.FLICKR_API_KEY
-    let resourceUrl = `https://api.flickr.com/services/rest/?`;
+		let flickrQueryParams = axelVisar(params);
+		let flickrUrl = `${resourceUrl}${flickrQueryParams}`;
 
-    let params = {
-      method: 'flickr.photos.search',
-      api_key: flickrAPIkey,
-      text: input,
-      sort: 'relevance',
-      extras: 'original_format, url_o, url_q',
-      license: '2,4,5,6,7',
-      per_page: 10,
-      format: 'json',
-      nojsoncallback: 1,
-      dimension_search_mode: "max"
-    };
-
-    let flickrQueryParams = axelVisar(params);
-    let flickrUrl = `${resourceUrl}${flickrQueryParams}`
-
-    return fetch(flickrUrl)
-      .then(res => res.json())
-      .then(res => {
-        console.log(res.photos);
-      });
-    }
+		return fetch(flickrUrl).then(res => res.json()).then(res => {
+			this.renderFlickrImages(res.photos.photo);
+		});
+	}
 }
-
-    
-
-   
-
-	
-
 
 (function() {
 	let go = new MashedApi(document.querySelector('#page'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,52 @@
+const webpack = require('webpack');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: "./src/js/app.js",
-    output: {
-        filename: './dist/bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
+	entry: './src/js/app.js',
+	output: {
+		filename: './dist/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
 
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    watch: true,
-    plugins: [
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            server: {
-                baseDir: ['dist']
-            },
-            files: ['./dist/*.html']
-        }),
-        new Dotenv({
-            path: './.env', // Path to .env file (this is the default) 
-            safe: false // load .env.example (defaults to "false" which does not use dotenv-safe) switch true false to reveal in console
-        })
-    ]
-}
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	watch: true,
+	plugins: [
+		new UglifyJSPlugin({
+			uglifyOptions: {
+				mangle: true,
+				minimize: true,
+				output: {
+					comments: false,
+					beautify: false
+				}
+			}
+		}),
+		new BrowserSyncPlugin({
+			host: 'localhost',
+			port: 3000,
+			server: {
+				baseDir: ['dist']
+			},
+			files: ['./dist/*.html']
+		}),
+		new Dotenv({
+			path: './.env', // Path to .env file (this is the default)
+			safe: false // load .env.example (defaults to "false" which does not use dotenv-safe) switch true false to reveal in console
+		})
+	]
+};
